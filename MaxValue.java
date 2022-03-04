173ff16f12613ac2c5fe0c98ef044b6d22905c6f
unknown <saiprakashbeemari@gmail.com>
2022-03-04 16:19:48 +0530

    String Maxvalue

--- MaxValue.java
+++ MaxValue.java
@@ -20,7 +20,7 @@ public class MaxValue
 
 		  public static void main(String args[]) 
 		  {
-		    System.out.printf("MaxValue of float values  %f, %f and %f is %f\n\n", 2.5,3.2, 8.6, maximum(2.5,3.2 ,8.6));
+		    System.out.printf("MaxValue of String  of  %s, %s and %s is %s\n\n", "Peach","Apple", "Banana", maximum("Peach","Apple" ,"Banana"));
 		    
 		  }
 		  
