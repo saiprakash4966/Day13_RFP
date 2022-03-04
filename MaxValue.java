406cbd738f438feec5b893c060474887a9767615
unknown <saiprakashbeemari@gmail.com>
2022-03-04 17:14:38 +0530

    Sort elements of all types

--- MaxValue.java
+++ MaxValue.java
@@ -1,49 +1,82 @@
 package com.bl;
 
+import java.util.Arrays;
+
 public class MaxValue
 {
-	
-	 
-			public static void main(String[] args) 
-			{
+	public static void main(String[] args) 
+	{
+		Integer aI = 5, bI = 3, cI = 2;
+		Float aF = 2.2f, bF = 6.5f, cF = 9.3f;
+		String aS = "Apple", bS = "Peach", cS = "Banana";
 
-				Integer aI = 10, bI = 20, cI = 30;
-				Float aF = 2.2f, bF = 3.6f, cF = 7.3f;
-				String aS = "Apple", bS = "Peach", cS = "Banana";
+		Integer[] arrInt = { aI,bI,cI };
+		Float[] arrFlt = { aF, bF, cF };
+		String[] arrStr = { aS, bS, cS };
 
-				/*
-				 * calling methods for finding Maximum
-				 */
-				
-				
-				testMaximum(aI,bI,cI);
-				testMaximum(aF,bF,cF);
-				testMaximum(aS,bS,cS);
-			}
+		System.out.println("Before sort");
+		printArray(arrInt);
+		printArray(arrFlt);
+		printArray(arrStr);
 
-			/*
-			 * Generic method of Type Integer,Float,String for finding maximum of 3
-			 */
-			private static <S extends Comparable<S>> S testMaximum(S a, S b, S c)
-			{
-				S max;
-				if (a.compareTo(b) == 1 && a.compareTo(c) == 1) {
-					max = a;
-				} else if (b.compareTo(a) == 1 && b.compareTo(c) == 1) {
-					max = b;
-				} else {
-					max = c;
-				}
-				printMax(a, b, c, max);
+		/*
+		 * sorting of an array
+		 */
+		Arrays.sort(arrInt);
+		Arrays.sort(arrFlt);
+		Arrays.sort(arrStr);
 
-				return max;
-			}
+		printArraySort(arrInt);
+		printArraySort(arrFlt);
+		printArraySort(arrStr);
 
-			public static <T> void printMax(T x, T y, T z, T max) {
-				System.out.printf("max of %s,%s and %s is :- %s\n", x, y, z, max);
-			}
+		System.out.println();
+		testMaximum(aI, bI, cI);
+		testMaximum(aF, bF, cF);
+		testMaximum(aS, bS, cS);
 
-		  
-		  
+	}
+
+	private static <T> void printArraySort(T[] arr) {
+		System.out.println("Sorted array[] is : " + Arrays.toString(arr));
+
+	}
+
+	/*
+	 * Before sorting
+	 */
+	public static <S> void printArray(S[] arr) {
+		Integer i;
+		for (i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
+		System.out.println();
+	}
 
+	/*
+	 * Testing the maximum value
+	 */
+	private static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
+		T max;
+
+		if (x.compareTo(y) == 1 && x.compareTo(z) == 1) {
+			max = x;
+
+		} else if (y.compareTo(x) == 1 && y.compareTo(z) == 1) {
+			max = y;
+
+		} else {
+			max = z;
+		}
+		printMax(x, y, z, max);
+
+		return max;
+	}
+
+	/*
+	 * Printing the maximum value
+	 */
+	public static <T> void printMax(T x, T y, T z, T max) {
+		System.out.printf("max of %s,%s and %s is %s\n", x, y, z, max);
+	}
+}
